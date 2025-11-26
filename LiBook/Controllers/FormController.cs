243b8fc02563bfe31c77ed815390c9542f8a5243
moira <moirachelseyburbos@gmail.com>
2025-11-26 15:04:58 +0800

    feat(user-ui): add student model and post method

--- LiBook/Controllers/FormController.cs
+++ LiBook/Controllers/FormController.cs
@@ -1,16 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using LiBook.Models; // Make sure to include your Models namespace
+
 
 namespace LiBook.Controllers
 {
     public class FormController : Controller
     {
+        // GET methods
         public ActionResult StudentView() => View();
         public ActionResult FacultyView() => View();
         public ActionResult AdminView() => View();
         public ActionResult VisitorView() => View();
+
+        // POST method for the Student form
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult StudentView(StudentViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            System.Diagnostics.Debug.WriteLine($"Student: {model.LastName}, {model.FirstName}, {model.MiddleInitial}");
+            System.Diagnostics.Debug.WriteLine($"Student Number: {model.StudentNumberPrefix}-{model.StudentNumberSuffix}");
+            System.Diagnostics.Debug.WriteLine($"Email: {model.Email}");
+            System.Diagnostics.Debug.WriteLine($"Program: {model.Program}");
+            System.Diagnostics.Debug.WriteLine($"Purpose: {model.Purpose}");
+            System.Diagnostics.Debug.WriteLine($"Date: {model.Date.ToShortDateString()}");
+            System.Diagnostics.Debug.WriteLine($"Time: {model.TimeStart} - {model.TimeEnd}");
+            System.Diagnostics.Debug.WriteLine($"Number of Members: {model.NumberOfMembers}");
+
+            if (model.Members != null)
+            {
+                var membersList = string.Join(", ", model.Members);
+                System.Diagnostics.Debug.WriteLine(membersList);
+            }
+
+            TempData["SuccessMessage"] = "Form submitted successfully!";
+            return RedirectToAction("StudentView");
+        }
     }
 }
---
+++ LiBook/Models/StudentViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LiBook.Models
+{
+    public class StudentViewModel
+    {
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public string MiddleInitial { get; set; }
+        public string Suffix { get; set; }
+        [Required]
+        public string StudentNumberPrefix { get; set; }
+        [Required]
+        public string StudentNumberSuffix { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Purpose { get; set; }
+        [Required]
+        public string Program { get; set; }
+        [Required, DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        [Required]
+        public string TimeStart { get; set; }
+        [Required]
+        public string TimeEnd { get; set; }
+        [Required]
+        public int NumberOfMembers { get; set; }
+        public List<string> Members { get; set; } = new List<string>();
+
+    }
+}
